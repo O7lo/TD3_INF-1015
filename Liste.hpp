9ede11e76fc352009c5363c47df4935893d36d02
Arthur Panoyan <arthur.panoyan@polymtl.ca>
2021-10-18 14:54:48 -0400

    ajout du constructeur, du surcharge d'operateur et de la fonction d'ajout de element. Ces fonctions ne sont pas testes.

--- Liste.hpp
+++ Liste.hpp
@@ -10,9 +10,22 @@ class Liste
 {
 public:
 	//TODO: Constructeurs et surcharges d'opérateurs
+	Liste() {}
+	~Liste() { std::cout << "Detrut!" << endl; }
+
+	Liste& operator[] (const Liste& indice)
+	{
+		return elements_[indice];
+	}
+
 
 	//TODO: Méthode pour ajouter un élément à la liste
-
+	void ajouterJeu(T liste, std::unique_ptr<T> element)
+	{
+		if (nElements_ == capacite_)
+			changerCapacite(liste,capacite_ * 2);
+		elements_[nElements_++] = element;
+	} 
 	// Pour size, on utilise le même nom que les accesseurs de la bibliothèque standard, qui permet d'utiliser certaines fonctions de la bibliotheque sur cette classe.
 	unsigned size() const         { return nElements_; }
 	unsigned getCapacite() const  { return capacite_; }
@@ -20,8 +33,8 @@ public:
 	//TODO: Méthode pour changer la capacité de la liste
 	void changerCapacite(T liste, unsigned nouvelleCapacite) {
 		if (nouvelleCapacite > nElements_) {
-			auto nouvelleListe = std:make_unique<T[]>(nouvelleCapacite);
-			for (unsigned i : range(nElements_)) {
+			auto nouvelleListe = std::make_unique<T[]>(nouvelleCapacite);
+			for (unsigned i : iter::range(nElements_)) {
 				nouvelleListe[i] = elements_[i];
 			}
 			elements_ = nouvelleListe;
@@ -31,9 +44,10 @@ public:
 
 	//TODO: Méthode pour trouver une élément selon un critère (lambda).
 
+
 private:
 	unsigned nElements_;
 	unsigned capacite_;
 	//TODO: Attribut contenant les éléments de la liste.
-	std:unique_ptr<T[]> elements_;
+	std::unique_ptr<T[]> elements_;
 };
