3191aab2327e46713e7c4c6bd2f1a72b5f0cd7db
O7lo <80862226+O7lo@users.noreply.github.com>
2021-10-18 13:35:29 -0400

    Add files via upload

---
+++ Liste.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include <cassert>
+#include "gsl/span"
+#include "cppitertools/range.hpp"
+
+template <typename T>
+class Liste
+{
+public:
+	//TODO: Constructeurs et surcharges d'opérateurs
+
+	//TODO: Méthode pour ajouter un élément à la liste
+
+	// Pour size, on utilise le même nom que les accesseurs de la bibliothèque standard, qui permet d'utiliser certaines fonctions de la bibliotheque sur cette classe.
+	unsigned size() const         { return nElements_; }
+	unsigned getCapacite() const  { return capacite_; }
+
+	//TODO: Méthode pour changer la capacité de la liste
+	void changerCapacite(T liste, unsigned nouvelleCapacite) {
+		if (nouvelleCapacite > nElements_) {
+			auto nouvelleListe = std:make_unique<T[]>(nouvelleCapacite);
+			for (unsigned i : range(nElements_)) {
+				nouvelleListe[i] = elements_[i];
+			}
+			elements_ = nouvelleListe;
+			capacite_ = nouvelleCapacite;
+		}
+	}
+
+	//TODO: Méthode pour trouver une élément selon un critère (lambda).
+
+private:
+	unsigned nElements_;
+	unsigned capacite_;
+	//TODO: Attribut contenant les éléments de la liste.
+	std:unique_ptr<T[]> elements_;
+};
